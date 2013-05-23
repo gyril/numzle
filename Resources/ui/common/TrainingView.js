4c8f0af334fa0dc28fb3c5c0ee108729f34e8677
gyril <cyril.gantzer@gmail.com>
2013-05-23 19:29:14 +0200

    v0.2

--- Resources/ui/common/TrainingView.js
+++ Resources/ui/common/TrainingView.js
@@ -29,55 +29,97 @@ function TrainingView() {
 		return shuffle(grid);
 	}
 	
-	function generateNumbers() {
-		var tgrid = grid;
-		var d1 = tgrid[Math.floor(Math.random()*tgrid.length)];
-		tgrid.splice(tgrid.indexOf(d1), 1);
-		var d2 = tgrid[Math.floor(Math.random()*tgrid.length)];
-		tgrid.splice(tgrid.indexOf(d2), 1);
+	function generateNumbers(grid) {
+		var tgrid = grid.slice(0, grid.length-1);
+		var number = tgrid[Math.floor(Math.random()*tgrid.length)];
+		tgrid.splice(tgrid.indexOf(number), 1);
+		var operands = 2 + Math.floor(Math.random()*3);
 		
-		return [d1 * d2];
+		for (var i = 1; i < operands; i++) {
+			var operator = Math.floor(Math.random()*3);
+			var d = tgrid[Math.floor(Math.random()*tgrid.length)];
+			tgrid.splice(tgrid.indexOf(number), 1);
+			switch(operator) {
+				case 0:
+					number += d;
+					break;
+				case 1:
+					number -= d;
+					break;
+				case 2:
+					number *= d;
+					break;
+				default:
+					console.log(operand);
+			}
+		}
+		console.log(grid);
+		return [number];
+	}
+	
+	function refreshProposition() {
+		propDisplay.text = proposition;
+		if(numbers[0] == parseInt(proposition)) {
+			alert("Gagné");
+			numbers = generateNumbers(grid);
+			objectif.text = String.format(L('find', "Find "+numbers[0]),numbers[0]);
+			proposition = 0;
+			operation = 0;
+			propDisplay.text = "";
+		}
 	}
 	
 	var plusSign = Ti.UI.createButton({
 			color:'#000000',
 			title:"+",
-			height:50,
-			width:50,
-			visible:false,
-			zIndex:1
+			height:100,
+			width:100,
+			visible:true,
+			zIndex:1,
+			top: 220,
+			left: 360
 		}),
 		minusSign = Ti.UI.createButton({
 			color:'#000000',
 			title:"-",
-			height:50,
-			width:50,
-			visible:false,
-			zIndex:1
+			height:100,
+			width:100,
+			visible:true,
+			zIndex:1,
+			top: 330,
+			left: 360
 		}),
 		multiplySign = Ti.UI.createButton({
 			color:'#000000',
 			title:"x",
-			height:50,
-			width:50,
-			visible:false,
-			zIndex:1
+			height:100,
+			width:100,
+			visible:true,
+			zIndex:1,
+			top: 440,
+			left: 360
 		}),
-		divideSign = Ti.UI.createButton({
-			color:'#000000',
-			title:"/",
-			height:50,
-			width:50,
-			visible:false,
-			zIndex:1
+		propDisplay = Ti.UI.createLabel({
+			color:'#ffffff',
+			font: { fontSize:26 },
+			text:"",
+			height:'auto',
+			width:'auto',
+			top:'150px'
 		});
 		
 	self.add(plusSign);
 	self.add(minusSign);
 	self.add(multiplySign);
-	self.add(divideSign);
+	self.add(propDisplay);
 	
-	var grid = generateGrid(12, [6,4,2]);
+	plusSign.addEventListener("touchend", function() { operation = 0; }, false);
+	minusSign.addEventListener("touchend", function() { operation = 1; }, false);
+	multiplySign.addEventListener("touchend", function() { operation = 2; }, false);
+	
+	var proposition = 0;
+	var operation = 0;
+	var grid = generateGrid(9, [5,4,0]);
 	
 	for(var i in grid) {
 		
@@ -88,42 +130,40 @@ function TrainingView() {
 			width:100,
 			top:220+Math.floor(i/3)*110,
 			left:20+i%3*110,
-		backgroundImage:'/images/case.png'
+			backgroundImage:'/images/case.png'
 		});
 		
 		(function(digit) {
-			digit.addEventListener("touchstart", function() {
+			digit.addEventListener("touchend", function() {
 				
-				function touchend() {
-					self.removeEventListener("touchend", touchend);
-					plusSign.visible = false;
-					minusSign.visible = false;
-					multiplySign.visible = false;
-					divideSign.visible = false;
+//				function touchend() {
+//					self.removeEventListener("touchend", touchend);
+//				}
+				console.log(digit.title);
+				switch(operation) {
+					case 0:
+						proposition += parseInt(digit.title);
+						break;
+					case 1:
+						proposition -= parseInt(digit.title);
+						break;
+					case 2:
+						proposition *= parseInt(digit.title);
+						break;
+					default:
+						console.log("default");
 				}
 				
-				plusSign.top = digit.getTop() - 55;
-				plusSign.left = digit.getLeft() + 25;
-				minusSign.top = digit.getTop() + 105;
-				minusSign.left = digit.getLeft() + 25;
-				multiplySign.top = digit.getTop() + 25;
-				multiplySign.left = digit.getLeft() + 105;
-				divideSign.top = digit.getTop() + 25;
-				divideSign.left = digit.getLeft() - 55;
-
-				plusSign.visible = true;
-				minusSign.visible = true;
-				multiplySign.visible = true;
-				divideSign.visible = true;
+				refreshProposition();
 				
-				self.addEventListener("touchend", touchend);
+//				self.addEventListener("touchend", touchend);
 			});
 		})(digit)
 		
 		self.add(digit);
 	}
 	
-	var numbers = generateNumbers();
+	var numbers = generateNumbers(grid);
 
 	var objectif = Ti.UI.createLabel({
 		color:'#ffffff',
@@ -145,6 +185,33 @@ function TrainingView() {
 		right:'10px'
 	});
 	self.add(timer);
+	
+	var validate = Ti.UI.createButton({
+		backgroundColor: '#00FF00',
+		color:'#FFFFFF',
+		title:"OK",
+		height:50,
+		width:150,
+		top:660,
+		left:250
+	});
+	self.add(validate);
+	
+	var cancel = Ti.UI.createButton({
+		backgroundColor: '#FF0000',
+		color:'#FFFFFF',
+		title:"X",
+		height:50,
+		width:150,
+		top:660,
+		left:100
+	});
+	self.add(cancel);
+	cancel.addEventListener("touchend", function() {
+		proposition = 0;
+		operation = 0;
+		refreshProposition();
+	}, false);
 	
 	var endTime = (new Date()).getTime() + 120*1000;
 	var setTimer = setInterval(function() {
